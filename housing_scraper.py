4a504274f4a24a26347facd0cf3f9815b0401169
DartLazer <71255041+DartLazer@users.noreply.github.com>
2023-08-22 16:59:56 +0200

    House scriper code reformat and update

--- housing_scraper.py
+++ housing_scraper.py
@@ -7,71 +7,136 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 
-sleep_interval_timer = 900 # sleep interval timer in seconds
+sleep_interval_timer = 900  # sleep interval timer in seconds
 
 # Read the website configurations
 config = configparser.ConfigParser(interpolation=None)
 config.read('config.cfg')
 
-# Read the Telegram configurations
-telegram_config = configparser.ConfigParser()
-telegram_config.read('telegram.cfg')
-telegram_token = telegram_config['telegram']['token']
-telegram_chat_id = telegram_config['telegram']['chat_id']
+
+def read_stored_tag(filename: str) -> str | None:
+    """
+    Reads a stored tag value from a predefined local text file. This so states are preserved in between sessions
+    :param filename: filename/filepath of the file.
+    :return: contents of the file as string
+    """
+    if os.path.exists(filename):
+        with open(filename, 'r') as file:
+            return file.read().strip()
+    return None
 
 
-def notify_telegram(message: str, token: str, chat_id: str) -> bool:
-    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
+def write_stored_tag(filename: str, tag: str) -> None:
+    """
+    Writes a new tag value from a website to a local text file.
+    :param filename: new filename/filepath
+    :param tag: Contents of the tag
+    :return: None
+    """
+    with open(filename, 'w') as file:
+        file.write(tag)
+
+
+def handle_new_tag_found(url: str, latest_tag: str) -> None:
+    """
+    Actions to perform when a new tag is found.
+    :param url: URL at which the new tag is found
+    :param latest_tag: Conents of the new tag
+    :return:
+    """
+    print('New apartment!')
+    message = f"New apartment found on {url}: {latest_tag}"
+    telegram_status = notify_telegram(message)
+    if not telegram_status:
+        print('Error sending telegram message')
+
+
+def notify_telegram(message: str) -> bool:
+    """
+    Send a notification to the user via Telegram with a predfined message
+    :param message: String containing the message
+    :return: If a 200 code was received from telegram
+    """
+    telegram_config = configparser.ConfigParser()
+    telegram_config.read('telegram.cfg')
+    telegram_token = telegram_config['telegram']['token']
+    telegram_chat_id = telegram_config['telegram']['chat_id']
+    url = f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={telegram_chat_id}&text={message}"
     response = requests.get(url)
     return response.status_code == 200
 
 
-def fetch_latest_apartment(url: str, html_tag: str, html_class: str) -> str:
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537'
-    }
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    tag = soup.find(html_tag, class_=html_class)
+def find_newest_tag_in_page(soup_page: BeautifulSoup, target_html_tag: str, target_html_class: str):
+    """
+    Scans a beautiful soup object for a taget html tag and target html class
+    :param soup_page: The page that was just visited and converted to a bs4 object
+    :param target_html_tag: html tag to look for
+    :param target_html_class: html class to look for
+    :return: The stripped string of the found html tag, or else None
+    """
+    tag = soup_page.find(target_html_tag, class_=target_html_class)
+
+    # Return the desired value (modify as needed)
     return tag.text.strip() if tag else None
 
 
-def fetch_latest_apartment_with_selenium(url: str, html_tag: str, html_class: str):
+def fetch_website(url: str) -> BeautifulSoup | None:
+    """
+    Use python requests module to obtain a specific website. Convert it to a BS4 object
+    :param url: Target url
+    :return:
+    """
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                      '(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537'
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an exception if the HTTP status code is 4xx or 5xx
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return soup
+    except requests.RequestException as e:
+        print(f"An error occurred while fetching the URL {url}: {e}")
+        return None
+
+
+def fetch_website_using_selenium(url: str) -> BeautifulSoup | None:
+    """
+    Use Selenium to visit a website and convert its contents to a bs4 object
+    :param url: target URL
+    :return: Bs4 object or None depending on the success of reaching the website.
+    """
+
     options = Options()
     options.add_argument('--headless')
-    #options.add_argument(
-    #    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537")
-    # Linux User agent
     options.add_argument(
-        "user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537")
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537")
 
     driver = webdriver.Chrome(options=options)
     try:
         driver.get(url)
         # Explicitly wait for the element to become present on the page
         WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located
+            ec.presence_of_all_elements_located
         )
 
         # Get the page source and parse it with BeautifulSoup
         soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-        # Find the tag using the given HTML tag and class
-        tag = soup.find(html_tag, class_=html_class)
-        # Don't forget to close the WebDriver
         driver.quit()
 
-        # Return the desired value (modify as needed)
-        return tag.text.strip() if tag else None
-    except Exception as e:
-        print(f'Error occured while using selenium:{e}')
+        return soup
+    except Exception as error_message:
+        print(f'Error occurred while using selenium:{error_message}')
         driver.quit()
         return None
 
 
 def main():
+    print('Checking all websites for new entries...')
     # Loop through config file site sections
     for site in config.sections():
         url = config[site]['url']
@@ -81,35 +146,25 @@ def main():
         use_selenium = config[site].get('use_selenium', 'no').lower() == 'yes'
 
         if use_selenium:
-            print(f'Fetching {site} using selenium... Beetje geduld...')
-            latest_apartment = fetch_latest_apartment_with_selenium(url, html_tag, html_class)
+            soup_page = fetch_website_using_selenium(url)
         else:
-            print(f'Fetching {site}...')
-            latest_apartment = fetch_latest_apartment(url, html_tag, html_class)
+            soup_page = fetch_website(url)
 
-        if latest_apartment is None:
-            print(f"Failed to fetch latest apartment for site: {site}")
+        latest_tag = find_newest_tag_in_page(soup_page, html_tag, html_class)
+
+        if latest_tag is None:
+            message = f"ERROR!!! Unable to find tag {html_tag} and class {html_class} on website: {site}"
+            print(message)
+            notify_telegram(message)
             continue
 
-        # Check memory files for last scanned appartments
-        if os.path.exists(filename):
-            with open(filename, 'r') as file:
-                stored_apartment = file.read().strip()
+        previous_stored_tag = read_stored_tag(filename)
 
-            # New appartment if last name is not new name, could also be that an appartment is sold. Lets see how
-            # this plays out
-            if stored_apartment != latest_apartment:
-                print('New appartment!')
-                message = f"New apartment found on {url}: {latest_apartment}"
-                telegram_status = notify_telegram(message, telegram_token, telegram_chat_id)
-                if not telegram_status:
-                    print('Error sending telegram message')
+        if previous_stored_tag and previous_stored_tag != latest_tag:
+            print(f'New listing found on {site} at {url}!')
+            handle_new_tag_found(url, latest_tag)
 
-            elif stored_apartment == latest_apartment:
-                print(f'No New Appartments on {site}')
-
-        with open(filename, 'w') as file:
-            file.write(latest_apartment)
+        write_stored_tag(filename, latest_tag)
 
 
 if __name__ == "__main__":
@@ -117,6 +172,6 @@ if __name__ == "__main__":
         try:
             main()
         except Exception as e:
-            print(f'An error has occured {e}')
+            print(f'An error has occurred {e}')
         print(f'Script finished sleeping for {sleep_interval_timer}')
         sleep(sleep_interval_timer)
